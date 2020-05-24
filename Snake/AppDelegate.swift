86889aa4150f5d8f245e39ce47140ad8ad7ca4d0
Adnan Zahid <adnan.zahid@dubizzle.com>
2020-05-24 08:43:17 +0400

    Initial Commit

---
+++ Snake/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Snake
+//
+//  Created by Adnan Zahid on 24/05/2020.
+//  Copyright © 2020 Snake. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
